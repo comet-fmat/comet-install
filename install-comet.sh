10536a5b0be3801ca963680cf17307cf54024f2c
Ángel Iván Díaz Noriega <angel.diaz@live.com.mx>
2018-01-17 22:55:22 -0600

    no se logra mandar la password desde rvmsudo :( y con sudo no lee las gemas

--- install-comet.sh
+++ install-comet.sh
@@ -7,35 +7,39 @@ else
     echo "You should be 'tmc' user and stay logged in"
 	exit 1
 fi
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:keyz182/multistrap-forceyes-fix
+
+echo -n Password: 
+read -s password
+
+echo $password | sudo -S add-apt-repository -y ppa:webupd8team/java
+echo $password | sudo -S add-apt-repository -y ppa:keyz182/multistrap-forceyes-fix
 
 echo "Update your package list..."
-sudo apt-get update
-sudo apt-get -y upgrade
+echo $password | sudo -S apt-get update
+echo $password | sudo -S apt-get -y upgrade
 
 echo "TMC-server dependencies"
-sudo apt-get -y install git build-essential zip unzip imagemagick maven make phantomjs bc postgresql postgresql-contrib chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev xfonts-75dpi libpq-dev
+echo $password | sudo -S apt-get -y install git build-essential zip unzip imagemagick maven make phantomjs bc postgresql postgresql-contrib chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev xfonts-75dpi libpq-dev
 
 echo "Ruby dependencies"
-sudo apt-get -y install git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev
+echo $password | sudo -S apt-get -y install git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev
 
 echo "RVM dependencies"
-sudo apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+echo $password | sudo -S apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 
 echo "TMC-sandbox dependencies"
-sudo apt-get -y install squashfs-tools multistrap e2fsprogs e2tools
+echo $password | sudo -S apt-get -y install squashfs-tools multistrap e2fsprogs e2tools
 
 echo "Java installation"
-sudo apt-get -y install oracle-java8-installer
+echo $password | sudo -S apt-get -y install oracle-java8-installer
 
 echo "Install RVM as multi-user install"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | sudo bash -s stable
+\curl -sSL https://get.rvm.io | sudo -S bash -s stable
 source /etc/profile.d/rvm.sh
 
 echo "Add yourself to RVM group and install Ruby"
-sudo adduser tmc rvm
+echo $password | sudo -S adduser tmc rvm
 
 echo "Install ruby"
 rvm install 2.2.0
@@ -43,12 +47,12 @@ rvm use 2.2.0 --default
 gem install bundler
 
 echo "Create postgres user"
-sudo -H -u postgres psql -c "CREATE USER tmc PASSWORD 'tmc' CREATEDB CREATEROLE CREATEUSER;"
+echo $password | sudo -S -H -u postgres psql -c "CREATE USER tmc PASSWORD 'tmc' CREATEDB CREATEROLE CREATEUSER;"
 
 echo "TMC-server installation"
 git clone git@github.com:comet-fmat/tmc-server.git /home/tmc/tmc-server
 cd /home/tmc/tmc-server
-#sudo apt-get -y install rake bundler
+#echo $password | sudo -S apt-get -y install rake bundler
 bundle install
 git submodule update --init --recursive
 bundle exec rake db:reset
@@ -56,22 +60,26 @@ bundle exec rake db:reset
 
 echo "Build sandbox"
 cd ext/tmc-sandbox
-sudo make
+echo $password | sudo -S make
 
 echo "Install tmc-sandbox web dependencies"
 cd web
+echo "export rvmsudo -S_secure_path=1" >> /home/tmc/.bash_profile
+source /home/tmc/.bash_profile
+rvm install "ruby-2.2.2"
+rvm use 2.2.2
 bundle install
 rake ext
-rvmsudo rake test
+echo "tmc" | rvmsudo -S rake test
 
 echo "Compile rest of the externals"
 cd -
 rake compile
 
 echo "Install tmc-check"
-sudo apt-get install check
+echo $password | sudo -S apt-get install check
 cd tmc-sandbox/uml/output/tmc-check
-rvmsudo make rubygems install clean
+rvmecho $password | sudo -S make rubygems install clean
 
 
 
